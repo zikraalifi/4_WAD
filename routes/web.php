3ab939bbcd777cd6368d5bd5cd6364d797f8beca
zakki1705 <zakisumanta@gmail.com>
2025-06-01 22:51:23 +0700

    commit 7

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MedicineController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('medicines', MedicineController::class);
