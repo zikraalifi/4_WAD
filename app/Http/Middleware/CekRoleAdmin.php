7d2106604650da3397f09f16f2b5558b5a9ea23f
zikraalifi <zikraalifi111@gmail.com>
2025-06-01 23:18:58 +0700

    update

---
+++ app/Http/Middleware/CekRoleAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CekRoleAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role === 'admin') {
+            return $next($request);
+        }
+
+        return redirect('/dashboard')->with('error', 'Akses ditolak. Hanya admin yang bisa masuk.');
+    }
+}
